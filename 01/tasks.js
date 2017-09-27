ad7f7d3b6a9241baa8d1f3cfcd2760a85e076c9f
arbik <bikbaev.airat@gmail.com>
2017-09-27 20:23:46 +0300

    first homework

--- 01/tasks.js
+++ 01/tasks.js
@@ -5,7 +5,11 @@
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
 function getMinMax(string) {
+  var regex = /(0[xX][+-]?[\dabcdefABCDEF]+)|([-]?\d*\.?\d+(?:[Ee]?[+-]?\d+)?)/g;
+  var numbers = string.match(regex);
 
+  numbers = numbers.sort((a, b) => Number(a) - Number(b));
+  return { min: numbers[0], max: numbers[numbers.length - 1] };
 }
 
 /* ============================================= */
@@ -16,7 +20,7 @@ function getMinMax(string) {
  * @return {number} число под номером х
  */
 function fibonacciSimple(x) {
-  return x;
+  return x > 1 ? fibonacciSimple(x - 1) + fibonacciSimple(x - 2) : x;
 }
 
 /* ============================================= */
@@ -27,8 +31,14 @@ function fibonacciSimple(x) {
  * @param {number} x номер числа
  * @return {number} число под номером х
  */
+var results = [0, 1];
+
 function fibonacciWithCache(x) {
-  return x;
+  if (x === 0) { return 0; }
+  if (x === 1) { return 1; }
+  if (!results[x]) { results[x] = fibonacciWithCache(x - 1) + fibonacciWithCache(x - 2); }
+
+  return results[x];
 }
 
 /* ============================================= */
@@ -49,7 +59,37 @@ function fibonacciWithCache(x) {
  * @return {string}
  */
 function printNumbers(max, cols) {
+  var answer = '',
+    rowCount = Math.ceil((max + 1) / cols),
+    numbOnAdditionalRow = (max + 1) % cols,
+    result = new Array(rowCount);
 
+  for (var i = 0; i < rowCount; i++) {
+    result[i] = new Array(cols);
+    var dimension = cols,
+      count = 0;
+
+    if (i === rowCount - 1 && numbOnAdditionalRow > 0) { dimension = numbOnAdditionalRow; }
+
+    for (var j = 0; j < dimension; j++) {
+      if (j > numbOnAdditionalRow && numbOnAdditionalRow !== 0) {
+        result[i][j] = (i + count - 1);
+        count--;
+      } else { result[i][j] = (i + count); }
+
+      count += rowCount;
+    }
+  }
+  result.forEach(arr => {
+    arr.forEach(a => {
+      a > 9 ? answer += `${a} ` : answer += ` ${a} `;
+    });
+    answer = answer.substr(0, answer.length - 1);
+    answer += '\n';
+  });
+  answer = answer.substr(0, answer.length - 1);
+
+  return answer;
 }
 
 /* ============================================= */
@@ -60,7 +100,14 @@ function printNumbers(max, cols) {
  * @return {string}
  */
 function rle(input) {
+  var result = [];
+  var regex = /(.)\1*/g;
 
+  input.match(regex).forEach(group => {
+    group.length > 1 ? result.push(group[0], group.length) : result.push(group[0]);
+  });
+
+  return result.join('');
 }
 
 module.exports = {
