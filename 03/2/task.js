7679a1b96c75cc71f185ce3efdee0bcb4d4e290a
arbik <bikbaev.airat@gmail.com>
2017-10-11 01:16:10 +0300

    Some changes for ESlint

--- 03/2/task.js
+++ 03/2/task.js
@@ -1,14 +1,14 @@
-﻿/**
-* Сделать функцию, которая reject'ит возвращаемый промис, передавая в качестве ошибки строку 'timeout_error',
-* если он не resolve'ится за указанный timeout, или ведет себя эквивалентно исходному.
-* В учебных целях для этой задачи просьба не использовать Promise.race.
-*
-* @param {Promise} promise исходный промис
-* @param {Number} timeoutInMilliseconds время для timeout в миллисекундах
-* @return {Promise} промис с нужным поведением
-*/
+/**
+ * Сделать функцию, которая reject'ит возвращаемый промис, передавая в качестве ошибки строку 'timeout_error',
+ * если он не resolve'ится за указанный timeout, или ведет себя эквивалентно исходному.
+ * В учебных целях для этой задачи просьба не использовать Promise.race.
+ *
+ * @param {Promise} promise исходный промис
+ * @param {Number} timeoutInMilliseconds время для timeout в миллисекундах
+ * @return {Promise} промис с нужным поведением
+ */
 function rejectOnTimeout(promise, timeoutInMilliseconds) {
-   return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     promise.then(resolve, reject);
     setTimeout(() => {
       reject('timeout_error');
